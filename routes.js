6db9f84e295a45123ba06e38d36430faf73b2b42
brunobordond <bruno.daniel.bordon.diaz.uptp@gmail.com>
2022-08-09 14:11:04 -0400

    nuevo sistema

--- routes.js
+++ routes.js
@@ -11,13 +11,25 @@ const path = require('path');
 const util = require('util')
 const copyFile = util.promisify(fs.copyFile);
 
+var QrScanner = require('qr-scanner');
+
 router.get('/', async function (req, res) {
     res.render('index')
 
 })
 
-router.post('/uploadqr', upload.none(), function (req, res) {
-    
+router.post('/uploadqr', upload.none(), async function (req, res) {
+    const base64Data = req.body.image;
+    fs.writeFile("test.png", base64Data, 'base64', function (err) {
+        console.log('::'+err)
+    })
+
+    var buffer = fs.readFileSync('./test.png');
+    console.log(buffer)
+
+    // QrScanner.scanImage(buffer)
+    //     .then(result => console.log('result'))
+    //     .catch(error => console.log('No QR code found.'));
 })
 
 module.exports = router;
